e0c13d9a36738c7872949e753698fa220198f4dc
Vraj Bhavsar <vraj137@hotmail.com>
2023-02-21 20:20:58 -0500

    cleaned up code

--- src/App.js
+++ src/App.js
@@ -9,45 +9,54 @@ import {
   Badge,
   Flex,
   Select,
-
-  // Th,
-  // Thead,
-  // Tr,
 } from "@chakra-ui/react";
 import Modal from "react-modal";
 import { Input } from "@chakra-ui/react";
 
-
-
 function App(props) {
+
+  // const declarations 
   const [events, setEvents] = useState([]);
   const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [eventType, setEventType] = useState('');
+  const [eventType, setEventType] = useState("");
   const [isOpen, setIsOpen] = useState(false);
-  const eventTypes = [...new Set(events.map(event => event.event_type))];
+  const eventTypes = [...new Set(events.map((event) => event.event_type))];
 
-
+  // login modal opens when logging in or logging out 
   function openModal() {
     setIsOpen(true);
   }
 
+  // close modal after logged in or out
   function closeModal() {
     setIsOpen(false);
   }
 
-  const filteredEvents = events.filter(event => {
+  // related events from link 
+  function RelatedEvent(props) {
+    return (
+      <a href={`https://api.hackthenorth.com/v3/events/${props.id}`}>
+        {props.name}
+      </a>
+    );
+  }
+
+  // filter events
+  const filteredEvents = events.filter((event) => {
     if (!eventType) {
       return true;
     }
     return event.event_type === eventType;
   });
 
+    // tab title 
   useEffect(() => {
     document.title = "2021 Hackathon Global Inc."; // Set the tab title
   }, []);
 
+  // gets events from link
   useEffect(() => {
     fetch("https://api.hackthenorth.com/v3/events")
       .then((response) => response.json())
@@ -56,6 +65,7 @@ function App(props) {
       });
   }, []);
 
+  // handles log in 
   function handleLogin(event) {
     event.preventDefault();
     if (username === "myusername" && password === "mypassword") {
@@ -66,11 +76,12 @@ function App(props) {
     }
   }
 
+  // handles logout 
   function handleLogout() {
     setLoggedIn(false);
     closeModal();
   }
-
+  // capitalize event type and remove _ (underscore)
   function capitalizeEventType(eventType) {
     if (eventType.includes("_")) {
       eventType = eventType.replace("_", " ");
@@ -82,97 +93,115 @@ function App(props) {
     <div className="App">
       <Box className="content" p="2rem">
         <Card>
+          {/* title of the page */}
           <CardHeader>
             <h2 style={{ fontWeight: "bold", fontSize: "30px" }}>
               2021 Upcoming Events @ Hackathon Global Inc.
             </h2>
           </CardHeader>
 
+          {/* Filter options box */}
           <Flex justifyContent="center" mt="2">
-          <Box mr="2">
-            <Text fontWeight="bold">Filter by Event Type:</Text>
-          </Box>
-          <Box w="40%">
-            <Select value={eventType} onChange={e => setEventType(e.target.value)}>
-              <option value="">All</option>
-              {eventTypes.map(eventType => (
-                <option key={eventType} value={eventType}>{eventType}</option>
-              ))}
-            </Select>
-          </Box>
-        </Flex>
+            <Box mr="2">
+              <Text fontWeight="bold">Filter by Event Type:</Text>
+            </Box>
+            <Box w="40%">
+              <Select
+                value={eventType}
+                onChange={(e) => setEventType(e.target.value)}
+              >
+                <option value="">All</option>
+                {eventTypes.map((eventType) => (
+                  <option key={eventType} value={eventType}>
+                    {eventType}
+                  </option>
+                ))}
+              </Select>
+            </Box>
+          </Flex>
 
-        {!loggedIn && (
-        <Button size="lg" margin="10" onClick={openModal}>
-          Log In To View All Events!
-        </Button>
-      )}
+          {/* Allows users to log in or log out depending on their status  */}
+          {!loggedIn && (
+            <Button size="lg" margin="10" onClick={openModal}>
+              Log In To View All Events!
+            </Button>
+          )}
 
-      {loggedIn && (
-        <Button margin="10" onClick={handleLogout}>
-          Log Out
-        </Button>
-      )}
+          {loggedIn && (
+            <Button margin="10" onClick={handleLogout}>
+              Log Out
+            </Button>
+          )}
 
-      <Modal isOpen={isOpen} onRequestClose={closeModal}>
-        {!loggedIn && (
-          <form onSubmit={handleLogin}>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "25vh",
-              }}
-            >
-              <h2>Login</h2>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-              }}
-            >
-              <label>
-                Username:
-                <Input
-                  pr="4.5rem"
-                  placeholder="Enter myusername"
-                  type={"username"}
-                  value={username}
-                  onChange={(e) => setUsername(e.target.value)}
-                  mt="3"
-                />
-              </label>
-              <label>
-                Password:
-                <Input
-                  mt="3"
-                  pr="4.5rem"
-                  type={"password"}
-                  placeholder="Enter mypassword"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </label>
-            </div>
-            <div
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                marginTop: "5vh",
-              }}
-            >
-              <Button type="submit">Log In</Button>
-            </div>
-          </form>
-        )}
-      </Modal>
-
-          <Flex mt = "4" flexWrap="wrap" flexDirection="row" justifyContent="center">
+          {/* checks to see when the modal is open and needs to be closed */}
+          <Modal isOpen={isOpen} onRequestClose={closeModal}>
+            {!loggedIn && (
+              <form onSubmit={handleLogin}>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "25vh",
+                  }}
+                >
+                  <h2>Login</h2>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "center",
+                  }}
+                >
+                  {/* user inputs for login */}
+                  <label>
+                    Username:
+                    <Input
+                      pr="4.5rem"
+                      placeholder="Enter myusername"
+                      type={"username"}
+                      value={username}
+                      onChange={(e) => setUsername(e.target.value)}
+                      mt="3"
+                    />
+                  </label>
+                  <label>
+                    Password:
+                    <Input
+                      mt="3"
+                      pr="4.5rem"
+                      type={"password"}
+                      placeholder="Enter mypassword"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </label>
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    marginTop: "5vh",
+                  }}
+                >
+                  {/* log in button  */}
+                  <Button type="submit">Log In</Button>
+                </div>
+              </form>
+            )}
+          </Modal>
+          
+          {/* allows the events to be sorted based on the data and time and shows only the ones that are public to regular users and private + public events if users are logged in*/}
+          <Flex
+            mt="4"
+            flexWrap="wrap"
+            flexDirection="row"
+            justifyContent="center"
+          >
             {filteredEvents
               .sort((a, b) => a.start_time - b.start_time)
               .map((event) => {
+                // checks to see the permission for the events from the link
                 const showPrivateEvent =
                   loggedIn && event.permission === "private";
                 const showRelatedEvents =
@@ -205,11 +234,9 @@ function App(props) {
                         </Badge>
                       </Box>
                       <Box mt="1" as="h4" lineHeight="tight">
-                        <Text fontWeight="bold">
-                          Event Type: 
-                        </Text>
-                        <Text color ="gray.500">
-                        {capitalizeEventType(event.event_type)}
+                        <Text fontWeight="bold">Event Type:</Text>
+                        <Text color="gray.500">
+                          {capitalizeEventType(event.event_type)}
                         </Text>
                       </Box>
                       <Box>
@@ -293,17 +320,9 @@ function App(props) {
           </Flex>
         </Card>
       </Box>
-
     </div>
   );
 }
 
-function RelatedEvent(props) {
-  return (
-    <a href={`https://api.hackthenorth.com/v3/events/${props.id}`}>
-      {props.name}
-    </a>
-  );
-}
 
 export default App;
